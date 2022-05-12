97ebc5252c0bca28db809ca865c1b51f35e60bd1
☕ Tim <tim@wants.coffee>
2022-05-12 14:03:51 +0800

    First cut at converting from observablehq to react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,159 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet";
+
+import {
+  CircleMarker,
+  MapContainer,
+  Polyline,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
+import { useAuspostLocations, useJoinedData, useOSMLocations } from "./hooks";
+
+import * as R from "ramda";
+
+import * as turf from "@turf/turf";
+import { useMemo, useState } from "react";
+
+const isPostOffice = (obj: any) =>
+  obj.properties.type === "PO" || obj.properties.amenity === "post_office";
+
+const isPostBox = (obj: any) =>
+  obj.properties.type === "R_SPB" ||
+  obj.properties.type === "C_SPB" ||
+  obj.properties.amenity === "post_box";
 
 function App() {
+  const { data: auspostData } = useAuspostLocations();
+  const { data: osmData } = useOSMLocations();
+
+  const [display, setDisplay] = useState<"post_office" | "post_box">(
+    "post_office"
+  );
+
+  const osmFeatureIndex = useMemo(
+    () => R.indexBy(({ id }) => id, osmData ? osmData.features : ([] as any)),
+    [osmData]
+  );
+
+  const [shownObject, setShownObject] = useState<any>();
+
+  const { auspostToOsm, osmToAuspost } = useJoinedData(
+    osmData as any,
+    auspostData,
+    { mergingDistance: display === "post_office" ? 0.5 : 0.1 }
+  );
+
+  const showFn = display === "post_office" ? isPostOffice : isPostBox;
+
+  const osmConnection =
+    typeof shownObject === "string" &&
+    [osmToAuspost.get(shownObject), osmFeatureIndex[shownObject]]
+      .filter((o) => !!o)
+      .map((o) => turf.centroid(o).geometry.coordinates.reverse());
+
+  const auspostConnection =
+    typeof shownObject === "object" &&
+    shownObject !== null &&
+    [shownObject, osmFeatureIndex[auspostToOsm.get(shownObject)]]
+      .filter((o) => !!o)
+      .map((o) => turf.centroid(o).geometry.coordinates.reverse());
+
+  const connection = osmConnection || auspostConnection;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select
+        style={{ position: "absolute", top: 10, right: 10, zIndex: 1000 }}
+        value={display}
+        onChange={(e) => setDisplay((e as any).target.value)}
+      >
+        <option value="post_office">Post Office</option>
+        <option value="post_box">Post Box</option>
+      </select>
+      <MapContainer
+        center={[-32.0499, 115.8086]}
+        zoom={15}
+        style={{ height: "100vh", width: "100wv" }}
+      >
+        <TileLayer
+          attribution={
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          }
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
+        />
+
+        {osmData &&
+          osmData.features.filter(showFn).map((feature, i) => {
+            const latLong = turf
+              .centroid(feature as any)
+              .geometry.coordinates.reverse();
+
+            const color = osmToAuspost.has(feature.id) ? "#36b436" : "#75cff0";
+
+            return (
+              <CircleMarker
+                key={`osm-${i}-${color}`}
+                center={latLong as any}
+                radius={10}
+                color={color}
+                weight={1}
+                opacity={1}
+                eventHandlers={{
+                  click: () => setShownObject(feature.id),
+                }}
+              >
+                <Popup>
+                  <b>OSM Data</b>
+                  <pre>{JSON.stringify(feature.properties, null, 2)}</pre>
+                </Popup>
+              </CircleMarker>
+            );
+          })}
+
+        {auspostData &&
+          auspostData.features.filter(showFn).map((feature: any, i) => {
+            const latLong = turf
+              .centroid(feature as any)
+              .geometry.coordinates.reverse();
+
+            const color = auspostToOsm.has(feature) ? "#36b436" : "#dc1928";
+
+            return (
+              <CircleMarker
+                key={`aus-${i}-${color}`}
+                center={latLong as any}
+                radius={10}
+                color={color}
+                weight={1}
+                opacity={1}
+                eventHandlers={{
+                  click: () => setShownObject(feature),
+                }}
+              >
+                <Popup>
+                  <b>AusPost Data</b>
+                  <pre>
+                    {JSON.stringify(
+                      R.omit(
+                        ["geo_location", "hours", "service_codes"],
+                        feature.properties
+                      ),
+                      null,
+                      2
+                    )}
+                  </pre>
+                </Popup>
+              </CircleMarker>
+            );
+          })}
+
+        {connection && connection.length === 2 && (
+          <Polyline positions={connection as any} />
+        )}
+      </MapContainer>
     </div>
   );
 }
---
+++ src/hooks.ts
@@ -0,0 +1,159 @@
+import { useQuery } from "react-query";
+import osmtogeojson from "osmtogeojson";
+
+import {
+  FeatureCollection,
+  featureCollection,
+  GeometryObject,
+  MultiPoint,
+  Point,
+  pointsWithinPolygon,
+  Properties,
+} from "@turf/turf";
+import { useMemo } from "react";
+
+import * as turf from "@turf/turf";
+
+import * as R from "ramda";
+
+const ONE_HOUR = 1000 * 60 * 60;
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+export const useAuspostLocations = () => {
+  return useQuery(
+    ["auspost"],
+    () => {
+      return fetch(
+        "https://cdn.jsdelivr.net/gh/Georift/auspost-locations/metro-perth-geojson.json"
+      )
+        .then((res) => res.json())
+        .then((geojson) =>
+          pointsWithinPolygon(geojson as any, boundingBox as any)
+        );
+    },
+    {
+      // this will rarely change, they aren't adding/removing stores that often
+      staleTime: ONE_DAY * 7,
+      cacheTime: Infinity,
+      placeholderData: featureCollection([]),
+    }
+  );
+};
+
+export const useOSMLocations = () => {
+  return useQuery(
+    ["overpass"],
+    () =>
+      fetch("https://overpass-api.de/api/interpreter", {
+        method: "POST",
+        body: `
+[out:json];
+(
+  nwr["amenity"="post_office"](-32.14567625378891,115.69255828857422,-31.87522527511162,116.08222961425781);
+  nwr["amenity"="post_box"](-32.14567625378891,115.69255828857422,-31.87522527511162,116.08222961425781);
+  nwr["amenity"="vending_machine"]["vending"="parcel_pickup"](-32.14567625378891,115.69255828857422,-31.87522527511162,116.08222961425781);
+);
+(._;>>;);
+out;`,
+      })
+        .then((res) => res.json())
+        .then((osm) => osmtogeojson(osm)),
+    {
+      staleTime: ONE_HOUR,
+      cacheTime: Infinity,
+      placeholderData: featureCollection([]),
+    }
+  );
+};
+
+const boundingBox = {
+  type: "Feature",
+  properties: {},
+  geometry: {
+    type: "Polygon",
+    coordinates: [
+      [
+        [115.69255828857422, -32.14567625378891],
+        [116.08222961425781, -32.14567625378891],
+        [116.08222961425781, -31.87522527511162],
+        [115.69255828857422, -31.87522527511162],
+        [115.69255828857422, -32.14567625378891],
+      ],
+    ],
+  },
+};
+
+/**
+ * "PO" = Post office
+ * "UPL" = Parcel Locker
+ * "R_SPB" = Red Post Box
+ * "C_SPB" = Express Post Box
+ * "PO C" = Parcel Collect
+ */
+export type AuspostFeatureType = "PO" | "UPL" | "R_SPB" | "C_SPB" | "PO C";
+
+export const useJoinedData = (
+  osmData: FeatureCollection<GeometryObject, any> | undefined,
+  auspostData: FeatureCollection<Point | MultiPoint, Properties> | undefined,
+  { mergingDistance }: { mergingDistance: number }
+) => {
+  return useMemo(() => {
+    const auspostToOsm = new Map();
+    const osmToAuspost = new Map();
+
+    const osmByType = R.groupBy(
+      ({ properties }) => (properties ? properties.amenity : "undefined"),
+      osmData ? osmData.features : []
+    );
+
+    console.log(osmByType);
+
+    const auspostTypeToOSMType = {
+      PO: "post_office",
+      UPL: "vending_machine",
+      R_SPB: "post_box",
+      C_SPB: "post_box",
+    };
+
+    (auspostData ? auspostData.features : [])
+      .filter((feature) =>
+        Object.keys(auspostTypeToOSMType).includes(
+          (feature.properties as any).type
+        )
+      )
+      .forEach((feature: any) => {
+        const auspostType: AuspostFeatureType = feature.properties.type;
+
+        // @ts-ignore
+        const amenity = auspostTypeToOSMType[auspostType];
+        if (amenity === undefined) {
+          return;
+        }
+
+        const osmFeatures = (osmByType[amenity] || [])
+          .filter((f) => !osmToAuspost.has(f.id))
+          .map((f) => ({ ...turf.centroid(f as any), id: f.id }));
+
+        if (osmFeatures.length === 0) {
+          return;
+        }
+
+        const nearest = turf.nearest(
+          feature,
+          turf.featureCollection(osmFeatures)
+        );
+
+        if (!nearest) {
+          return;
+        }
+
+        // less than half a kilometer
+        if (turf.distance(nearest, feature) <= mergingDistance) {
+          auspostToOsm.set(feature, nearest.id);
+          osmToAuspost.set(nearest.id, feature);
+        }
+      });
+
+    return { auspostToOsm, osmToAuspost };
+  }, [osmData, auspostData, mergingDistance]);
+};
