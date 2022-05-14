ee41893a4ce65989250f5b472dfe80b4e53f5a06
☕ Tim <tim@wants.coffee>
2022-05-14 15:17:40 +0800

    Add download of OSM data
    Also renders the POI's using a similar approach to ID. Currently only
    showing Maki icons, will need to also bring in the font-awesome and
    temaki icons as well.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet";
 
 import LinearProgress from "@mui/material/LinearProgress";
+import DownloadIcon from "@mui/icons-material/Download";
+
+import * as L from "leaflet";
 
 import {
   CircleMarker,
   MapContainer,
+  Marker,
   Polyline,
   Popup,
   TileLayer,
@@ -19,7 +23,12 @@ import * as R from "ramda";
 import * as turf from "@turf/turf";
 import { useMemo, useState } from "react";
 import { CurrentLocationMarker } from "./CurrentLocationMarker";
-import { MenuItem, Select } from "@mui/material";
+import { CircularProgress, Fab, MenuItem, Select } from "@mui/material";
+import osmtogeojson from "osmtogeojson";
+import { useQuery } from "react-query";
+
+import { DivIconMarker } from "./DivIconMarker";
+import { IDMapMarker } from "./IDMapMarker";
 
 const isPostOffice = (obj: any) =>
   obj.properties.type === "PO" || obj.properties.amenity === "post_office";
@@ -70,6 +79,49 @@ function App() {
 
   const connection = osmConnection || auspostConnection;
 
+  const { data: osmQueryData, isLoading: isLoadingOsmApi } = useQuery(
+    ["osmDataAgain"],
+    () => {
+      return fetch(
+        "https://api.openstreetmap.org/api/0.6/map?bbox=115.80551147460938,-32.053326175386076,115.80830097198486,-32.05200764122336",
+        {
+          headers: { Accept: "application/json" },
+        }
+      )
+        .then((res) => res.json())
+        .then((osmJson) => osmtogeojson(osmJson))
+        .then((osmGeoJson) => {
+          return turf.featureCollection(
+            osmGeoJson.features.filter(({ id }) =>
+              (typeof id === "string" ? id : "").startsWith("node/")
+            ) as any
+          );
+        });
+    },
+    {
+      // the user should manually trigger this
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  );
+
+  const { data: presetDefaults } = useQuery(
+    ["presetDefaults"],
+    () => {
+      return fetch(
+        "https://cdn.jsdelivr.net/npm/@openstreetmap/id-tagging-schema@3/dist/presets.min.json"
+      ).then((res) => res.json());
+    },
+    {
+      // the user should manually trigger this
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  );
+
+  console.log(osmQueryData);
+  console.log(presetDefaults);
+
   return (
     <div>
       {isLoading && <LinearProgress />}
@@ -82,89 +134,148 @@ function App() {
         <MenuItem value="post_office">Post Office</MenuItem>
         <MenuItem value="post_box">Post Box</MenuItem>
       </Select>
+
+      <Fab
+        color="primary"
+        size="small"
+        sx={{ position: "absolute", left: 10, bottom: 75 }}
+      >
+        {!isLoadingOsmApi ? (
+          <DownloadIcon />
+        ) : (
+          <CircularProgress size="small" />
+        )}
+      </Fab>
+
       <MapContainer
         center={[-32.0499, 115.8086]}
         zoom={15}
+        maxZoom={25}
         style={{ height: "100vh", width: "100wv" }}
         zoomControl={false}
       >
-        <TileLayer
-          attribution={
-            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          }
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
-        />
+        <>
+          <TileLayer
+            attribution={
+              '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
+            maxNativeZoom={18}
+            maxZoom={25}
+          />
 
-        <CurrentLocationMarker />
+          {osmQueryData &&
+            osmQueryData.features.map((f, i) => {
+              const foundIcon = R.toPairs(f.properties || {})
+                .map(([key, value]) => `${key}/${value}`)
+                .map((k) => presetDefaults[k])
+                .filter(
+                  (o) =>
+                    typeof o === "object" &&
+                    o !== null &&
+                    typeof o.icon === "string" &&
+                    o.icon.startsWith("maki-")
+                );
 
-        {osmData &&
-          osmData.features.filter(showFn).map((feature, i) => {
-            const latLong = turf
-              .centroid(feature as any)
-              .geometry.coordinates.reverse();
+              return (
+                <DivIconMarker
+                  key={i}
+                  marker={{
+                    position: [...f.geometry.coordinates].reverse() as any,
+                  }}
+                  container={{ tagName: "svg" }}
+                  divIconProps={{
+                    className: "leaflet-id-icon",
+                    iconSize: [18, 24],
+                    iconAnchor: [9, 25],
+                  }}
+                >
+                  <IDMapMarker
+                    iconId={
+                      foundIcon.length > 0
+                        ? `${foundIcon[0].icon}-15`
+                        : undefined
+                    }
+                  />
+                </DivIconMarker>
+              );
+            })}
 
-            const color = osmToAuspost.has(feature.id) ? "#36b436" : "#75cff0";
+          <CurrentLocationMarker />
 
-            return (
-              <CircleMarker
-                key={`osm-${i}-${color}`}
-                center={latLong as any}
-                radius={10}
-                color={color}
-                weight={1}
-                opacity={1}
-                eventHandlers={{
-                  click: () => setShownObject(feature.id),
-                }}
-              >
-                <Popup>
-                  <b>OSM Data</b>
-                  <pre>{JSON.stringify(feature.properties, null, 2)}</pre>
-                </Popup>
-              </CircleMarker>
-            );
-          })}
+          {osmData &&
+            osmData.features.filter(showFn).map((feature, i) => {
+              const latLong = turf
+                .centroid(feature as any)
+                .geometry.coordinates.reverse();
 
-        {auspostData &&
-          auspostData.features.filter(showFn).map((feature: any, i: number) => {
-            const latLong = turf
-              .centroid(feature as any)
-              .geometry.coordinates.reverse();
+              const color = osmToAuspost.has(feature.id)
+                ? "#36b436"
+                : "#75cff0";
 
-            const color = auspostToOsm.has(feature) ? "#36b436" : "#dc1928";
+              return (
+                <CircleMarker
+                  key={`osm-${i}-${color}`}
+                  center={latLong as any}
+                  radius={10}
+                  color={color}
+                  weight={1}
+                  opacity={1}
+                  eventHandlers={{
+                    click: () => setShownObject(feature.id),
+                  }}
+                >
+                  <Popup>
+                    <b>OSM Data</b>
+                    <pre>{JSON.stringify(feature.properties, null, 2)}</pre>
+                  </Popup>
+                </CircleMarker>
+              );
+            })}
 
-            return (
-              <CircleMarker
-                key={`aus-${i}-${color}`}
-                center={latLong as any}
-                radius={10}
-                color={color}
-                weight={1}
-                opacity={1}
-                eventHandlers={{
-                  click: () => setShownObject(feature),
-                }}
-              >
-                <Popup>
-                  <b>AusPost Data</b>
-                  <pre>
-                    {JSON.stringify(
-                      R.omit(
-                        ["geo_location", "hours", "service_codes"],
-                        feature.properties
-                      ),
-                      null,
-                      2
-                    )}
-                  </pre>
-                </Popup>
-              </CircleMarker>
-            );
-          })}
+          {auspostData &&
+            auspostData.features
+              .filter(showFn)
+              .map((feature: any, i: number) => {
+                const latLong = turf
+                  .centroid(feature as any)
+                  .geometry.coordinates.reverse();
 
-        {connection && connection.length === 2 && (
-          <Polyline positions={connection as any} />
-        )}
+                const color = auspostToOsm.has(feature) ? "#36b436" : "#dc1928";
+
+                return (
+                  <CircleMarker
+                    key={`aus-${i}-${color}`}
+                    center={latLong as any}
+                    radius={10}
+                    color={color}
+                    weight={1}
+                    opacity={1}
+                    eventHandlers={{
+                      click: () => setShownObject(feature),
+                    }}
+                  >
+                    <Popup>
+                      <b>AusPost Data</b>
+                      <pre>
+                        {JSON.stringify(
+                          R.omit(
+                            ["geo_location", "hours", "service_codes"],
+                            feature.properties
+                          ),
+                          null,
+                          2
+                        )}
+                      </pre>
+                    </Popup>
+                  </CircleMarker>
+                );
+              })}
+
+          {connection && connection.length === 2 && (
+            <Polyline positions={connection as any} />
+          )}
+        </>
       </MapContainer>
     </div>
   );
---
+++ src/IDMapMarker.tsx
@@ -0,0 +1,29 @@
+export const IDMapMarker = ({ iconId }: { iconId?: string }) => {
+  return (
+    <svg
+      style={{
+        height: 25,
+        width: 19,
+      }}
+    >
+      <g transform="translate(9, 24)">
+        {/* Adds a little circle on the ground under the marker, I don't think we need this for anything */}
+        {/*<ellipse cx="0.5" cy="1" rx="6.5" ry="3" className="stroke"></ellipse>*/}
+        <path
+          className="stroke"
+          transform="translate(-8, -23)"
+          d="M 17,8 C 17,13 11,21 8.5,23.5 C 6,21 0,13 0,8 C 0,4 4,-0.5 8.5,-0.5 C 13,-0.5 17,4 17,8 z"
+        ></path>
+        {iconId && (
+          <use
+            transform="translate(-5, -19)"
+            className="icon"
+            width="11px"
+            height="11px"
+            xlinkHref={`#${iconId}`}
+          ></use>
+        )}
+      </g>
+    </svg>
+  );
+};
